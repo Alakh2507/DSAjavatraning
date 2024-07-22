a03549e877e45942183c15a1f3f120b3a5aec3ef
alakh niranjan <alakh.kushwaha1@gmil.com>
2024-07-22 12:02:14 +0530

    Add sum of two numbers

---
+++ Addoftowno.java
@@ -0,0 +1,10 @@
+package DSAjavatraning;
+
+public class Addoftowno {
+    public static void main(String args[]) {
+        int a = 5;
+        float b = (float) (7.5);
+        float c = a + b;
+        System.out.println(c);
+    }
+}
