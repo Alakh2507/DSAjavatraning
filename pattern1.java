1c5be46c56972fbb1e77b8b2a42a121c8181abe4
alakh niranjan <alakh.kushwaha1@gmil.com>
2024-07-24 01:27:39 +0530

    pattern demo

---
+++ pattern1.java
@@ -0,0 +1,58 @@
+package DSAjavatraning;
+
+public class pattern1 {
+    public static void forloop() {
+        for (int i = 1; i <= 8; i++)// row
+        {
+            for (int j = 1; j <= i; j++)// column
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+
+        }
+
+    }
+
+    public static void whileloop() {
+        int i = 1;
+        int j = 1;
+        while (i <= 5)// row
+        {
+            while (j <= i)// column
+            {
+                System.out.print("*");
+                j++;
+
+            }
+            i++;
+            System.out.println();
+            j = 1;
+        }
+
+    }
+
+    public static void dowhileloop() {
+        int i = 1;
+        int j = 1;
+        do {
+            i++;
+            do {
+                j++;
+                System.out.print("*");
+
+            } while (j <= i);
+            System.out.println();
+            j = 1;
+        } while (i <= 5);
+    }
+
+    public static void main(String args[]) {
+        forloop();
+        System.out.println("whileloop");
+        whileloop();
+        System.out.println("dowhileloop");
+        dowhileloop();
+
+    }
+}
